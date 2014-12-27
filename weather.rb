db3e41d454c7b3373dca3a2899ef4c427a1cf28b
Kathy ODriscoll <kodriscoll.dev@gmail.com>
2014-12-27 14:38:48 -0800

    First commit for the weather app

---
+++ weather.rb
@@ -0,0 +1,31 @@
+require 'sinatra'
+require 'yahoo_weatherman'
+
+def get_weather(location)
+	client = Weatherman::Client.new
+	weather = client.lookup_by_location(location)
+
+	
+end	
+
+get '/' do 
+	erb :home	
+end
+
+post '/weather' do
+	location = params[:post]['location']
+
+	@condition = get_weather(location).condition['text']
+	@temp = get_weather(location).condition['temp']
+	
+
+	if @condition.downcase =='sunny'
+		erb :sunny
+	elsif @condition.downcase == 'cloudy'
+		erb :cloudy
+	elsif @condition.downcase == 'heavy rain'
+		erb :rainy
+	else erb :weather
+	end	
+end
+
